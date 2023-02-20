73570a0107af5fc5c97dcc871a9e1aa080e664ba
Ismay1l <ismayilovismayil1226@gmail.com>
2023-02-20 21:36:57 +0400

    Page control implemented

---
+++ Yummy_App/Constants/Custom fonts.swift
@@ -0,0 +1,11 @@
+//
+//  Fonts.swift
+//  Yummy_App
+//
+//  Created by Ismayil Ismayilov on 20.02.23.
+//
+
+import UIKit
+
+//MARK: - Medium 
+let medium = UIFont(name: FontFamily.Labrada.medium.name, size: 22)
---
+++ Yummy_App/Extensions/Optional.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont.swift
+//  Yummy_App
+//
+//  Created by Ismayil Ismayilov on 20.02.23.
+//
+
+import UIKit
+
+extension UIFont {
+    var orEmpty: UIFont {
+        self 
+    }
+}
