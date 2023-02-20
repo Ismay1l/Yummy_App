8aedb056976656bf4e06751a62e32a090d733f0d
Ismay1l <ismayilovismayil1226@gmail.com>
2023-02-20 18:30:21 +0400

    First commit

---
+++ Yummy_App/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Yummy_App
+//
+//  Created by Ismayil Ismayilov on 19.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
